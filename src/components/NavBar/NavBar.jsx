87c6437e4c087d5682e0edeac8f438406b2a3605
Nathan Abela <nathanabela7@gmail.com>
2024-09-10 22:15:22 +0200

    refactor(navbar): wrap in fragment

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { AppBar, IconButton, Toolbar, Button, Avatar, useMediaQuery } from '@mui/material';
+import { AppBar, Avatar, Button, IconButton, Toolbar, useMediaQuery } from '@mui/material';
 import { Menu, AccountCircle, Brightness4, Brightness7 } from '@mui/icons-material';
 import { Link } from 'react-router-dom';
 import { useTheme } from '@mui/material/styles';
@@ -14,63 +14,65 @@ function NavBar() {
   const isAuthenticated = true; // TODO: Implement authentication logic
 
   return (
-    <AppBar position="fixed">
-      <Toolbar className={classes.toolbar}>
-        {/* Hamburger menu button for mobile screens */}
-        {isMobile && (
+    <>
+      <AppBar position="fixed">
+        <Toolbar className={classes.toolbar}>
+          {/* Hamburger menu button for mobile screens */}
+          {isMobile && (
+            <IconButton
+              color="inherit"
+              edge="start"
+              style={{ outline: 'none' }}
+              onClick={() => { }} // TODO: Implement menu functionality
+              className={classes.menuButton}
+            >
+              <Menu />
+            </IconButton>
+          )}
+
+          {/* Dark/Light theme toggle button */}
           <IconButton
             color="inherit"
-            edge="start"
-            style={{ outline: 'none' }}
-            onClick={() => { }} // TODO: Implement menu functionality
-            className={classes.menuButton}
+            sx={{ ml: 1 }}
+            onClick={() => { }} // TODO: Implement theme toggle functionality
           >
-            <Menu />
+            {theme.palette.mode === 'dark' ? <Brightness7 /> : <Brightness4 />}
           </IconButton>
-        )}
 
-        {/* Dark/Light theme toggle button */}
-        <IconButton
-          color="inherit"
-          sx={{ ml: 1 }}
-          onClick={() => { }} // TODO: Implement theme toggle functionality
-        >
-          {theme.palette.mode === 'dark' ? <Brightness7 /> : <Brightness4 />}
-        </IconButton>
+          {/* Placeholder for search (non-mobile screens) */}
+          {!isMobile && 'Search...'}
 
-        {/* Placeholder for search (non-mobile screens) */}
-        {!isMobile && 'Search...'}
+          <div>
+            {/* Display login button if not authenticated */}
+            {!isAuthenticated ? (
+              <Button color="inherit" onClick={() => { }}> {/* TODO: Implement login logic */}
+                Login &nbsp; <AccountCircle />
+              </Button>
+            ) : (
+              <Button
+                color="inherit"
+                component={Link}
+                to="/profile/:id" // TODO: Dynamically set user profile ID
+                className={classes.linkButton}
+                onClick={() => { }} // TODO: Handle profile click
+              >
+                {/* Show 'My Movies' on larger screens */}
+                {!isMobile && <>My Movies &nbsp;</>}
+                <Avatar
+                  style={{ width: 30, height: 30 }}
+                  alt="Profile"
+                  src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png" // TODO: Replace with actual profile image
+                />
+              </Button>
+            )}
+          </div>
 
-        <div>
-          {/* Display login button if not authenticated */}
-          {!isAuthenticated ? (
-            <Button color="inherit" onClick={() => { }}> {/* TODO: Implement login logic */}
-              Login &nbsp; <AccountCircle />
-            </Button>
-          ) : (
-            <Button
-              color="inherit"
-              component={Link}
-              to="/profile/:id" // TODO: Dynamically set user profile ID
-              className={classes.linkButton}
-              onClick={() => { }} // TODO: Handle profile click
-            >
-              {/* Show 'My Movies' on larger screens */}
-              {!isMobile && <>My Movies &nbsp;</>}
-              <Avatar
-                style={{ width: 30, height: 30 }}
-                alt="Profile"
-                src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png" // TODO: Replace with actual profile image
-              />
-            </Button>
-          )}
-        </div>
+          {/* Placeholder for search (non-mobile screens) */}
+          {!isMobile && 'Search...'}
 
-        {/* Placeholder for search (non-mobile screens) */}
-        {!isMobile && 'Search...'}
-
-      </Toolbar>
-    </AppBar>
+        </Toolbar>
+      </AppBar>
+    </>
   );
 }
 
