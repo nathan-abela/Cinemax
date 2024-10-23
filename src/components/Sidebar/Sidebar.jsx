337e308e5029215d1e5a4d0a2a5f836079f47f39
Nathan Abela <nathanabela7@gmail.com>
2024-10-23 23:52:34 +0200

    feat(*): fetch genres list

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
-import { Divider, List, ListItem, ListItemText, ListSubheader } from '@mui/material';
+import { Box, CircularProgress, Divider, List, ListItem, ListItemText, ListSubheader } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { useTheme } from '@mui/styles';
 
 import useStyles from './styles';
+import { useGetGenresQuery } from '../../services/tmdb';
 
 // Logos for light and dark themes
 import lightLogo from '../../assets/Cinemax_Logo_Blue.svg';
 import darkLogo from '../../assets/Cinemax_Logo_Red.svg';
 
 // Mock data for categories and genres
-const mockCategories = [
-  { label: 'Popular', value: 'popular' },
-  { label: 'Top Rated', value: 'top_rated' },
-  { label: 'Upcoming', value: 'upcoming' },
-];
+// const mockCategories = [
+//   { label: 'Popular', value: 'popular' },
+//   { label: 'Top Rated', value: 'top_rated' },
+//   { label: 'Upcoming', value: 'upcoming' },
+// ];
 
 const mockGenres = [
   { label: 'Comedy', value: 'comedy' },
@@ -26,6 +27,10 @@ const mockGenres = [
 function Sidebar() {
   const theme = useTheme(); // Get current theme (dark/ light mode)
   const classes = useStyles(); // Import custom styles
+
+  const { data, isFetching } = useGetGenresQuery();
+
+  console.log('~ getGenres:', useGetGenresQuery());
 
   return (
     <>
@@ -43,13 +48,17 @@ function Sidebar() {
       {/* Categories List */}
       <List>
         <ListSubheader>Categories</ListSubheader>
-        {mockCategories.map(({ label, value }) => (
-          <Link key={value} className={classes.links} to="/">
+        {isFetching ? (
+          <Box display="flex" justifyContent="center">
+            <CircularProgress />
+          </Box>
+        ) : data.genres.map(({ name }) => (
+          <Link key={name} className={classes.links} to="/">
             <ListItem onClick={() => {}} button> {/* TODO: Add navigation */}
               {/* <ListItemIcon>
                 <img src={darkLogo} className={classes.genreImage} height={30} />
               </ListItemIcon> */}
-              <ListItemText primary={label} />
+              <ListItemText primary={name} />
             </ListItem>
           </Link>
         ))}
