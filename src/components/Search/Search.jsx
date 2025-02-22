38fb4853bec5c3dc86347fc3e6d76a9a693f2c64
Nathan Abela <nathanabela7@gmail.com>
2025-02-22 17:55:21 +0100

    feat(search): show search on homepage only

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { TextField, InputAdornment } from '@mui/material';
+import { useLocation } from 'react-router-dom';
 import { Search as SearchIcon } from '@mui/icons-material';
+import { InputAdornment, TextField } from '@mui/material';
 
 import { searchMovie } from '../../features/currentGenreOrCategory';
 import useStyles from './styles';
@@ -10,12 +11,17 @@ function Search() {
   const classes = useStyles();
   const [query, setQuery] = useState('');
   const dispatch = useDispatch();
+  const location = useLocation();
 
   const handleKeyDown = (event) => {
     if (event.key === 'Enter') {
       dispatch(searchMovie(query));
     }
   };
+
+  if (location.pathname !== '/' && location.pathname !== '/approved') {
+    return null;
+  }
 
   return (
     <div className={classes.searchContainer}>
