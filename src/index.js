7aad1927780951b4ff5ddd5df82a3f1a7674fbaa
Nathan Abela <nathanabela7@gmail.com>
2025-05-01 15:53:09 +0200

    fix(*): add `basename` for github pages deployment

--- src/index.js
+++ src/index.js
@@ -13,11 +13,15 @@ import './index.css';
 // Retrieve the root element from '/public/index.html'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+// Determine if the app is running in production mode
+const isProd = process.env.NODE_ENV === 'production';
+
 // Render the App component wrapped with ToggleColorMode for MUI theming
 root.render(
   <Provider store={store}>
     <ToggleColorMode>
-      <BrowserRouter>
+      {/* Using a basename in production for GitHub Pages deployment */}
+      <BrowserRouter basename={isProd ? '/Cinemax' : ''}>
         <App />
       </BrowserRouter>
     </ToggleColorMode>
